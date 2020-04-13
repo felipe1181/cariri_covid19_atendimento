d81173b2bcbfcbb32039d16d7d0752e6ab246f20
felipe henrique ferraresi <felip.ses@hotmail.com>
2020-04-13 17:40:21 -0300

    css index login corrigido margin-top, criação do esqueleto da pagina de inicio/dashboard do sistema

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import './index.css';
+import { Col, Container,Row,Nav, NavItem, NavLink} from 'reactstrap';
 
 export default function Dashboard(){
     return(
-        <>
-            <h1>Dashboard</h1>
-        </>
+        <Container className="themed-container" fluid={true}> 
+           <Nav className="nav-menu-top">
+                <NavItem>
+                <NavLink href="#">Link</NavLink>
+                </NavItem>
+                <NavItem>
+                <NavLink href="#">Link</NavLink>
+                </NavItem>
+                <NavItem>
+                <NavLink href="#">Another Link</NavLink>
+                </NavItem>
+                <NavItem>
+                <NavLink disabled href="#">Disabled Link</NavLink>
+                </NavItem>
+            </Nav> 
+            <Row className="content" row xs="2">
+                <Col className="menu-nav-left" sm={{size:3}} >
+                    <h1>teste</h1>
+                </Col> 
+                <Col className="content-data" sm={{size:9}} >
+                    <h1>content</h1>
+                </Col>
+            </Row> 
+        </Container> 
     );
 }
