8d83c696602c8bd273b20887d0e2db85fd145676
felipe henrique ferraresi <felip.ses@hotmail.com>
2020-04-14 17:51:23 -0300

    botões iniciais na dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Layout,Form, Input, Button,Card,Row,Col,Alert,PageHeader } from 'antd'; 
+import { Layout,Form, Input, Button,Card,Row,Col,Alert,PageHeader,Divider } from 'antd'; 
 import 'antd/dist/antd.css';
 import './index.css'; 
 
@@ -11,25 +11,68 @@ export default function Dashboard(){
       <Layout>
         <Header>
           <PageHeader
-            className="site-page-header"
-            title="Title" 
-            subTitle="This is a subtitle"
+            className="site-page-header" 
+            subTitle="Feito por Felipe Ferraresi"
           />
         </Header>
           <Content>
             <Row>
-              <Col className="content-card-dashboard" xs={24} sm={20} md={16} lg={12} xl={12}>
-                <Card title="Default size card" extra={<a href="#">More</a>} >
-                    <Row>
+              <Col className="content-card-dashboard" xs={24} sm={20} md={16} lg={12} xl={6}>
+                <Card title="Atendimento Cariri covid-19">
+                    <Row gutter={16}>
+                      <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                        <Button type="primary" block>
+                        <RightOutlined />
+                          Primary
+                        </Button>
+                      </Col>
                       <Col xs={24} sm={24} md={12} lg={8} xl={8}>
                         <Button type="primary" block>
                           Primary
                         </Button>
                       </Col>
-                      <Col xs={24} sm={24} md={12} lg={8} xl={8}>col-8</Col>
-                      <Col xs={24} sm={24} md={12} lg={8} xl={8}>col-8</Col>
+                      <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                        <Button type="primary" block>
+                          Primary
+                        </Button>
+                      </Col>  
                     </Row> 
-                    
+                    <Divider orientation="left" style={{ color: '#333'}}> </Divider>
+                    <Row gutter={16}>
+                      <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                        <Button type="primary" block>
+                          Primary
+                        </Button>
+                      </Col>
+                      <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                        <Button type="primary" block>
+                          Primary
+                        </Button>
+                      </Col>
+                      <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                        <Button type="primary" block>
+                          Primary
+                        </Button>
+                      </Col>  
+                    </Row> 
+                      <Divider orientation="left" style={{ color: '#333'}}> </Divider>
+                    <Row gutter={16}>
+                      <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                        <Button type="primary" block>
+                          Primary
+                        </Button>
+                      </Col>
+                      <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                        <Button type="primary" block>
+                          Primary
+                        </Button>
+                      </Col>
+                      <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                        <Button type="primary" block>
+                          Primary
+                        </Button>
+                      </Col>  
+                    </Row> 
                   </Card>
               </Col> 
             </Row> 
